4d036c7406f187d4cdefebcf78e96095cb056e2d
Maruska <solinma1@uhk.cz>
2025-04-27 09:41:49 +0200

    projekt 3 finalni verze

--- src/main/java/pro1/apiDataModel/SpecializationsList.java
+++ src/main/java/pro1/apiDataModel/SpecializationsList.java
@@ -3,5 +3,5 @@ package pro1.apiDataModel;
 import java.util.List;
 
 public class SpecializationsList {
-    public List<Specialization> prijimaciObor; //prijimaci obor protože to hází json
+    public List<Specialization> prijimaciObor;
 }
